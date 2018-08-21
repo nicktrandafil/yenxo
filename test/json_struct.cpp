7aad78dd06b367ce3916bb8deecfc7c01673c350
Nicolai Trandafil <nicktrandafil@gmail.com>
2018-08-21 06:08:58 +0300

    Typical usecase

---
+++ test/json_struct.cpp
@@ -0,0 +1,107 @@
+/*
+  MIT License
+
+  Copyright (c) 2018 Nicolai Trandafil
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+*/
+
+
+// tested
+#include <variant.hpp>
+#include <variant_mixins.hpp>
+
+// local
+#include <ostream_mixins.hpp>
+#include <comparison_mixins.hpp>
+
+// 3rd
+#include <catch2/catch.hpp>
+
+
+namespace {
+
+
+struct Hobby
+        : mixin::Var<Hobby>
+        , mixin::UpdateFromVar<Hobby>
+        , mixin::OStream<Hobby>
+        , mixin::EqualityComparison<Hobby> {
+
+    Hobby() : id(0) {}
+
+    Hobby(int id, std::string const& description)
+        : id(id), description(description)
+    {}
+
+    int id;
+    std::string description;
+};
+
+
+struct Person
+        : mixin::Var<Person>
+        , mixin::UpdateFromVar<Person>
+        , mixin::OStream<Person>
+        , mixin::EqualityComparison<Person> {
+
+    Person() : age(0) {}
+
+    Person(std::string const& name, int age, Hobby const& hobby)
+        : name(name), age(age), hobby(hobby)
+    {}
+
+    std::string name;
+    int age;
+    Hobby hobby;
+};
+
+
+} // namespace
+
+
+BOOST_HANA_ADAPT_STRUCT(Hobby, id, description);
+BOOST_HANA_ADAPT_STRUCT(Person, name, age, hobby);
+
+
+TEST_CASE("Check simple json to struct", "[json_struct]") {
+    auto const json = R"(
+        {
+            "name": "Efendi",
+            "age": 20,
+            "hobby": {
+                "id": 10,
+                "description": "Barista"
+            }
+        }
+    )";
+
+    Hobby const hobby{
+        10,
+        "Barista"
+    };
+
+    Person const expected{
+        "Efendi",
+        20,
+        hobby
+    };
+
+    REQUIRE(expected == Person::fromVariant(Variant::from(rapidjson::Document().Parse(json))));
+}
