c83a6d1e0c236149314fb111f35578dc651647fa
Nicolai Trandafil <nicktrandafil@gmail.com>
2018-09-30 18:28:29 +0300

    transitive dependency

--- cmake/rproject-config.cmake
+++ cmake/rproject-config.cmake
@@ -1,3 +1,5 @@
+include(CMakeFindDependencyMacro)
+find_dependency(type_safe)
 include(${CMAKE_CURRENT_LIST_DIR}/rproject-targets.cmake)
 set(rproject_LIBRARY rproject)
 set(rproject_LIBRARIES rproject)
