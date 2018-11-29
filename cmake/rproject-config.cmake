fa8fd32fba961493b7f5d7cac2cb788e0fa00372
Nicolai Trandafil <nicktrandafil@gmail.com>
2018-11-30 00:31:11 +0200

    make usable as part of source tree (in external)

--- cmake/rproject-config.cmake
+++ cmake/rproject-config.cmake
@@ -1,5 +1,8 @@
 include(CMakeFindDependencyMacro)
+
 find_dependency(type_safe)
+
 include(${CMAKE_CURRENT_LIST_DIR}/rproject-targets.cmake)
+
 set(rproject_LIBRARY rproject)
 set(rproject_LIBRARIES rproject)
