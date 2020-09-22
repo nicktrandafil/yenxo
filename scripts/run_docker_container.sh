465a33398e7557a27a57a5817319a9a4e63fd759
Nicolai Trandafil <nicktrandafil@gmail.com>
2020-09-22 12:55:00 +0300

    Provide Dockerfile.
    Eliminate 'external' because it didn't have all deps anyway.

---
+++ scripts/run_docker_container.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+IMG=$1
+shift
+
+if [ -z "$IMG" ]; then
+    docker images
+    exit 0
+fi
+
+CONTAINER=$(docker run --rm -d -P -it \
+	--user $(id -u):$(id -g) \
+	--workdir="/home/$USER" \
+	--volume="/etc/group:/etc/group:ro" \
+	--volume="/etc/passwd:/etc/passwd:ro" \
+	--volume="/etc/shadow:/etc/shadow:ro" \
+	-v ${HOME}:${HOME} \
+	-v $(dirname $SSH_AUTH_SOCK):$(dirname $SSH_AUTH_SOCK) \
+	-e SSH_AUTH_SOCK=$SSH_AUTH_SOCK \
+	-e DISPLAY \
+	-e "QT_X11_NO_MITSHM=1" \
+	--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+	--volume="$HOME/.Xauthority:/root/.Xauthority" \
+	--cap-add=SYS_PTRACE \
+	--privileged \
+	--shm-size="1g" \
+	--hostname="$IMG" \
+	$IMG "$@"
+)
+
+if [ -z $CONTAINER ]; then
+    echo Container start failed, ${IMG}
+    exit 1
+else
+    echo ${IMG} $CONTAINER
+    docker exec -it $CONTAINER /bin/bash -l
+fi
+
+exit 0
