f9634c55e54912a98193eb49230fd08deec97ca1
Nicolai Trandafil <nicktrandafil@gmail.com>
2021-01-05 21:04:40 +0200

    Patch type_safe to disable example building.

--- scripts/build_docker_image.sh
+++ scripts/build_docker_image.sh
@@ -6,4 +6,4 @@ BOOST_VERSION=1.65.1
 BOOST_URL="https://dl.bintray.com/boostorg/release/${BOOST_VERSION}/source/boost_${BOOST_VERSION//\./_}.tar.gz"
 BUILD_TYPE=Release
 
-docker build -t serialize --build-arg BOOST_VERSION=${BOOST_VERSION} --build-arg BUILD_TYPE=${BUILD_TYPE} --build-arg BOOST_URL=${BOOST_URL} .
+docker build -t serialize --build-arg BOOST_VERSION=${BOOST_VERSION} --build-arg BUILD_TYPE=${BUILD_TYPE} --build-arg BOOST_URL=${BOOST_URL} -f Dockerfile ..
