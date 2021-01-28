6e19feede593b10b7f038546b55be1b8991f927e
Nicolai Trandafil <nicktrandafil@gmail.com>
2021-01-28 13:22:13 +0200

    Use system boost in docker. Use clang-11.

--- scripts/build_docker_image.sh
+++ scripts/build_docker_image.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-BOOST_VERSION=1.65.1
-BOOST_URL="https://dl.bintray.com/boostorg/release/${BOOST_VERSION}/source/boost_${BOOST_VERSION//\./_}.tar.gz"
 BUILD_TYPE=Release
 
-docker build -t serialize --build-arg BOOST_VERSION=${BOOST_VERSION} --build-arg BUILD_TYPE=${BUILD_TYPE} --build-arg BOOST_URL=${BOOST_URL} -f Dockerfile ..
+docker build -t serialize --build-arg BUILD_TYPE=${BUILD_TYPE} -f Dockerfile ..
